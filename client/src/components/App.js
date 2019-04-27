ef6ab4c453250623f55f665b6ce238b17f6e1c55
Fredrik Stenmark <fl22bx@student.lnu.se>
2019-04-27 18:44:47 +0200

    react structure

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,11 @@
+
 import React from 'react'
 import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
+import { ToDoList } from './toDoList/ToDoList'
 import './App.css'
 
-const GET_MOVIES = gql`
+const TODOS = gql`
 query {
   hello
 }
@@ -11,12 +13,16 @@ query {
 
 export const App = () => {
   return (
-    <Query query={GET_MOVIES}>
-      {({ data }) => {
-        return (
-          <p>{data.hello}</p>
-        )
-      }}
-    </Query>
+    <div className='wrapper'>
+      <div className='header'>
+        <h1>ToDoList</h1>
+      </div>
+      <Query query={TODOS}>
+        {({ data }) => {
+          return <ToDoList data={data} />
+        }}
+      </Query>
+    </div>
+
   )
 }
---
+++ client/src/components/toDoList/ToDoList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './ToDoList.css'
+export const ToDoList = (props) => (
+  <div className='ToDoList'>
+    <div>ToDoList</div>
+    <p>{props.data.hello}</p>
+    <div>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+    </div>
+  </div>
+)
