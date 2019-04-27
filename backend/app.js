062c117141178e8089205d06c53c864a138dd55b
Fredrik Stenmark <fl22bx@student.lnu.se>
2019-04-27 16:15:38 +0200

    graphQL server

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const path = require('path')
+const { resolvers, typeDefs } = require('./src/GraphQL/GraphqlSchema')
+const { ApolloServer } = require('apollo-server-express')
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, '../client')))
 
+const server = new ApolloServer({ typeDefs, resolvers })
+server.applyMiddleware({ app })
+
 app.listen(port, () => {
-  console.log(`App listens to port ${port}`)
+  console.log(`App listens to port ${port}
+GraphQL endpoint: ${server.graphqlPath}`)
 })
