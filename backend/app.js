84c1a3dc5b5217464a9cce28bf9bc9691774533d
Fredrik Stenmark <fl22bx@student.lnu.se>
2019-04-27 13:31:13 +0200

    react and webpaack

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
-const app = require('express')()
+const express = require('express')
+const path = require('path')
+const app = express()
 const port = 3000
+
+app.use(express.static(path.join(__dirname, '../client')))
 
 app.listen(port, () => {
   console.log(`App listens to port ${port}`)
---
+++ client/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+
+}
